810997825380e0692914226e3a3ae6f93420e625
Louis Suo <louis.suo@myob.com>
2017-01-31 10:23:31 +1100

    add version number, refer to bootstrap

--- local_dev.sh
+++ local_dev.sh
@@ -1,4 +1,5 @@
 #/bin/bash
 
-docker build -t local_dev -f $(pwd)/Dockerfile_dev . \
+docker rm -f local_dev_server \
+&& docker build -t local_dev -f $(pwd)/Dockerfile_dev . \
 && docker run -tid -p 80:80 --name="local_dev_server" -v $(pwd):/var/www local_dev
--- public/index.php
+++ public/index.php
@@ -1,30 +1,34 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>Example</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>Php deployment</title>
 </head>
 <body>
 
-<?php
-            echo "Hi, I'm a PHP script!";
-
-        ?>
+<h2><?php
+    echo "Hi, I'm a PHP project! my version is 9";
+?>
+</h2>
 <br>
 
+<h3>A little calculate function</h3>
 <?php
 $x = 5 /* + 15 */ + 5;
-echo $x;
+echo "5 /* + 15 */ + 5 = " . $x;
 ?>
 
 
 <br>
 
+<h3>A little datetime function</h3>
 <?php
 $date = new DateTime();
 $stamp = $date->format('Y-m-d H:i:s');
 
-echo "The last release date is " . $stamp;
+echo "The current time is " . $stamp;
 ?>
+
 
 </body>
 </html>
