4f1f1db48771baa4d61acf1742504fd89c5f9159
Louis Suo <louis.suo@myob.com>
2017-02-03 15:42:22 +1100

    form example

---
+++ public/about.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: louis.suo
+ * Date: 2/3/17
+ * Time: 3:31 PM
+ */
+phpinfo();
---
+++ public/form.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<title>Php deployment</title>
+</head>
+<body>
+<div class="well">
+	<form action="action" method="post">
+	<div class="form-group">
+		<label for="name">You name</label>
+		<input type="text" class="form-control" id="name" name="name" placeholder="Your name">
+	</div>
+	<div class="form-group">
+		<label for="age">you age</label>
+		<input type="text" class="form-control" id="age" name="age" placeholder="How old are you">
+	</div>
+
+	<div class="checkbox">
+		<label>
+			<input type="checkbox"> Check me out
+		</label>
+	</div>
+	<button type="submit" class="btn btn-default">Submit</button>
+</form>
+</div>
+
+</body>
+</html>
